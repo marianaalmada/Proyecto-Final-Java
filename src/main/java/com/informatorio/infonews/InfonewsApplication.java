44bdd10d4d124e0a68415cee3f21dc9f711ee461
unknown <marianaalmada15@outlook.com>
2022-07-18 17:17:54 -0300

    initial commit

---
+++ src/main/java/com/informatorio/infonews/InfonewsApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.infonews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfonewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfonewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informatorio/infonews/InfonewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.infonews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfonewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
