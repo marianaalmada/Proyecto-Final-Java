1286c475fcf5ca3ab921794c178a560d25ac7497
unknown <marianaalmada15@outlook.com>
2022-08-02 15:05:48 -0300

    metodos deleteAuthor y modifyAuthor creados

--- src/main/java/com/informatorio/infonews/controller/AuthorController.java
+++ src/main/java/com/informatorio/infonews/controller/AuthorController.java
@@ -3,7 +3,10 @@ package com.informatorio.infonews.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +31,22 @@ public class AuthorController {
     public ResponseEntity<?> createAuthor(@RequestBody AuthorDTO authorDTO) {
         Author author = authorConverter.toEntity(authorDTO);
         author.setFullName(author.getFirstName() + " " + author.getLastName());
+        authorRepository.save(author);
+        return new ResponseEntity<>(authorConverter.toDto(author), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/author/{authorId}")
+    public ResponseEntity<?> deleteAuthor(@PathVariable Long authorId) {
+        authorRepository.deleteById(authorId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/author/{authorId}")
+    public ResponseEntity<?> modifyAuthor(@PathVariable Long authorId, @RequestBody AuthorDTO authorDTO) {
+        Author author = authorRepository.findById(authorId).get();
+        author.setFirstName(authorDTO.getFirstName());
+        author.setLastName(authorDTO.getLastName());
+        author.setFullName(authorDTO.getFirstName() + " " + authorDTO.getLastName());
         authorRepository.save(author);
         return new ResponseEntity<>(authorConverter.toDto(author), HttpStatus.CREATED);
     }
