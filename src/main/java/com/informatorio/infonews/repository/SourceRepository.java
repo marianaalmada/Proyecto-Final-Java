cff2b16ed6ce38cf99d1306fc1e0bdd2f572b3a0
unknown <marianaalmada15@outlook.com>
2022-08-09 16:50:31 -0300

    custom query creada

--- src/main/java/com/informatorio/infonews/repository/SourceRepository.java
+++ src/main/java/com/informatorio/infonews/repository/SourceRepository.java
@@ -1,5 +1,7 @@
 package com.informatorio.infonews.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.informatorio.infonews.domain.Source;
 @Repository
 public interface SourceRepository extends JpaRepository<Source, Long>{
     
+    List<Source> findByNameContains(String name);
 }
