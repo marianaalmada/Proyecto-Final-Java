04c294901597bd47b32b396583a440657d4a7468
unknown <marianaalmada15@outlook.com>
2022-08-10 21:17:57 -0300

    metodo para convertir una lista de Articles a una lista de ArticlesDTO creado

--- src/main/java/com/informatorio/infonews/converter/ArticleConverter.java
+++ src/main/java/com/informatorio/infonews/converter/ArticleConverter.java
@@ -1,5 +1,8 @@
 package com.informatorio.infonews.converter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 import com.informatorio.infonews.domain.Article;
@@ -21,6 +24,13 @@ public class ArticleConverter {
             article.getAuthor(),
             article.getSource()
         );
+    }
+
+    public List<ArticleDTO> toDTO(List<Article> articles) {
+        List<ArticleDTO> articlesDT0 = articles.stream()
+                .map(article -> this.toDTO(article))
+                .collect(Collectors.toList());
+        return articlesDT0;
     }
 
     public Article toEntity(ArticleDTO articleDTO) {
